e462c1fa260dd5fd4deda83ac9c27ef731733f3d
e-zoboli <enricozob@gmail.com>
2024-10-01 13:36:54 +0200

    feat: add Dockerfile, add docker compose

--- Crawler.Console/Helpers/FileSaveStrategy.cs
+++ Crawler.Console/Helpers/FileSaveStrategy.cs
@@ -7,7 +7,7 @@ public class FileSaveStrategy : ISaveStrategy
     public void Save(IEnumerable<HtmlResult?> htmlResults)
     {
         var parentDirectory = Directory.GetParent(Directory.GetCurrentDirectory())!.FullName;
-        var writingPath = Path.Combine(parentDirectory, "app", "results", "results.json");
+        var writingPath = Path.Combine(parentDirectory, "app","data", "results.json");
         var options = new JsonSerializerOptions { WriteIndented = true };
         var json = JsonSerializer.Serialize(htmlResults, options);
         File.WriteAllText(writingPath, json);
